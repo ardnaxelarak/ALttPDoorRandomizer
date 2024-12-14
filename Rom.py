7e952c0b9e142e702c8049eab3f1decd03f73753
codemann8 <codemann8@gmail.com>
2024-12-14 16:17:52 -0600

    Base ROM update - Improved psuedobots / music fixes

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '2d658871687c966f35dd9597a4195256'
+RANDOMIZERBASEHASH = 'a51679f0e31ee29f59247545d705992a'
 
 
 class JsonRom(object):
