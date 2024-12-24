e3e227f3d5c4f5826001db1c24fc9f978f742cee
codemann8 <codemann8@gmail.com>
2024-12-24 11:39:27 -0600

    Baserom update to allow textbox suppression for "this dungeon" items

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'a51679f0e31ee29f59247545d705992a'
+RANDOMIZERBASEHASH = '48d4747c281935f6f5803f13864d8d69'
 
 
 class JsonRom(object):
