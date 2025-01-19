94d870e2a4ac425f7bacba099882da78435ce695
codemann8 <codemann8@gmail.com>
2025-01-19 01:30:53 -0600

    Fixed incorrect ganon silvers hint

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'a84f59e5e76492f6a5693835823e6292'
+RANDOMIZERBASEHASH = 'b439d99c7f41914eedb9f4097ecc8cc7'
 
 
 class JsonRom(object):
