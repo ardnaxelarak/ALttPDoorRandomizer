e6d24db03e6a5c7cb3b299bde6ed3ef5f6688fa5
Kara Alexandra <ardnaxelarak@gmail.com>
2025-03-08 18:32:18 -0600

    Include playthrough in JSON spoiler

--- Main.py
+++ Main.py
@@ -147,15 +147,15 @@ def main(args, seed=None):
     if args.create_spoiler and not args.jsonout:
         world.spoiler.to_file(output_path('%s_Spoiler.txt' % outfilebase))
 
+    if not args.skip_playthrough:
+        logger.info('Calculating playthrough.')
+        create_playthrough(world)
+
     if args.json_spoiler:
         with open(output_path('%s_Spoiler.json' % outfilebase), 'w') as outfile:
             outfile.write(world.spoiler.to_json())
         with open(output_path('%s_Meta.json' % outfilebase), 'w') as outfile:
             outfile.write(json.dumps(world.spoiler.metadata))
-
-    if not args.skip_playthrough:
-        logger.info('Calculating playthrough.')
-        create_playthrough(world)
 
     if args.jsonout:
         print(json.dumps({**jsonout, 'spoiler': world.spoiler.to_json()}))
