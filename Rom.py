40bff27e6b4f9135ab032a2d25f02fef890bc93d
codemann8 <codemann8@gmail.com>
2025-05-04 09:27:29 -0500

    Various bug fixes for Follower Shuffle post-launch
    - Fix issue with infinite purple chest item get
    - Fix issue with Kiki running away on iframe ledge hops
    - Fix issue with bad follower gfx on screens with followers
    - Fix issue with Zelda appearing in conditional follower locations

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '1143daca64a1dbdb151339830dca37df'
+RANDOMIZERBASEHASH = 'a1d6e5902d2e33a3c440aca3983897a3'
 
 
 class JsonRom(object):
