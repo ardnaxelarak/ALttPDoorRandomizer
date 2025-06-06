a1e650976571973dc7e719dbde81dcd1b13a9f26
codemann8 <codemann8@gmail.com>
2025-06-05 20:53:37 -0500

    Reset MSU Resume on S+Q

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '516242b1897da709d9bad09e99d55819'
+RANDOMIZERBASEHASH = '382c34a221600dbc824c3a709dd9cccb'
 
 
 class JsonRom(object):
