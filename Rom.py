8a68965872b029beb4af07355001135dcd2a1a86
codemann8 <codemann8@gmail.com>
2025-10-15 18:31:15 -0500

    New fix for pot drops when at sprite limit

--- Rom.py
+++ Rom.py
@@ -43,7 +43,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = '1831c9d47b9614b3dac3a68499dcb273'
+RANDOMIZERBASEHASH = '5616ec09bb20de9a3d483ea2ca27bc4c'
 
 
 class JsonRom(object):
