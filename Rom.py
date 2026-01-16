e058fd1961713963c881e7b435a6d0fe5078309b
Kara Alexandra <ardnaxelarak@gmail.com>
2026-01-16 14:38:16 -0600

    Update baserom

--- Rom.py
+++ Rom.py
@@ -44,7 +44,7 @@ from source.enemizer.Enemizer import write_enemy_shuffle_settings
 
 
 JAP10HASH = '03a63945398191337e896e5771f77173'
-RANDOMIZERBASEHASH = 'a2c07b8d2b0dc7050102472e28469dfa'
+RANDOMIZERBASEHASH = 'deecff935f83b9050e7b9ef86231d607'
 
 
 class JsonRom(object):
